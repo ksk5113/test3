9197ae38a5fb5f0a592c777d8c2a653faecbd4a2
ksk5113 <kskksk5113@gmail.com>
2016-04-07 11:40:09 +0000

    test3

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,6 +2,8 @@ class TestController < ApplicationController
     def prov
         @age = [25,25,25,23,23 ,24,27,27,27, 28,24,24, 25,21,25,25,26, 26,26, 22,25,25,22, 23,23, 29,25]
         @age2 = (@age + [24, 24]).sort
+        @len = @age.length
+        @len2 = @age2.length
     end
     
     def prov2
